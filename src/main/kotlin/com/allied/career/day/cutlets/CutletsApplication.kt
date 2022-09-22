31c02b706be8e9fef1e96677db134e35b7e3b8bf
Edward Baranetsky <reader3dx@gmail.com>
2022-09-23 00:35:47 +0300

    init commit

---
+++ src/main/kotlin/com/allied/career/day/cutlets/CutletsApplication.kt
@@ -0,0 +1,11 @@
+package com.allied.career.day.cutlets
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CutletsApplication
+
+fun main(args: Array<String>) {
+	runApplication<CutletsApplication>(*args)
+}
---
+++ src/test/kotlin/com/allied/career/day/cutlets/CutletsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.allied.career.day.cutlets
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CutletsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
